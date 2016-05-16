1a15c5992e3ccf81f219d53c003b3d4bd8a5ca89
Matt Miller <mwm@fastmail.com>
2016-05-16 11:57:02 +0200

    Update dependencies.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Chacha20.Mixfile do
 
   def project do
     [app: :chacha20,
-     version: "0.3.1",
+     version: "0.3.2",
      elixir: "~> 1.2",
      name: "Chacha20",
      source_url: "https://github.com/mwmiller/chacha20_ex",
@@ -20,8 +20,8 @@ defmodule Chacha20.Mixfile do
 
   defp deps do
     [
-      {:earmark, ">= 0.0.0", only: :dev},
-      {:ex_doc, "~> 0.11.4", only: :dev},
+      {:earmark, "~> 0.2", only: :dev},
+      {:ex_doc, "~> 0.11", only: :dev},
       {:power_assert, "~> 0.0.8", only: :test},
     ]
   end
